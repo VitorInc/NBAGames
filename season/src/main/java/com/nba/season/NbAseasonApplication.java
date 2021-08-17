c2497c7dbd048fe82784f69c9e14700690825f55
VitorInc <vitordc.contato@gmail.com>
2021-08-17 19:09:34 -0300

    Project started

---
+++ season/src/main/java/com/nba/season/NbAseasonApplication.java
@@ -0,0 +1,13 @@
+package com.nba.season;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbAseasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbAseasonApplication.class, args);
+	}
+
+}
---
+++ season/src/test/java/com/nba/season/NbAseasonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.season;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbAseasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
